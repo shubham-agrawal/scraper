b71ae35a534e4b2dd6693e1e7e77fcc136584396
shubham-agrawal <mail2shubham001@gmail.com>
2015-07-13 18:27:29 +0530

    repair and maintaienance

--- indiamarttest.py
+++ indiamarttest.py
@@ -24,20 +24,26 @@ br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
 # User-Agent (this is cheating, ok?)
 br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-br.open('http://www.sourbook.com/zipavuj.php?G7eeqpILGqJt8oq2cI0jxA=Q6tQjN5FbYJdlaouGd0SIb%2B1L%2B4nPR6yTLvhceRjn%2BABh%2Bfb50HKaFXKNppeyWFQPM3cg9iPK%2FGc3Xo%2B2oZzCQ%3D%3D')
+br.open('http://www.sourbook.com/zipavuj.php?G7eeqpILGqJt8oq2cI0jxA=Q6tQjN5FbYJdlaouGd0SIZtmoQlUgU86MUeNLi71P8yn9daZZT3BIr3mUQNAd9Bxc0zuRoZOA3Qmy%2Fqil76lRg%3D%3D')
 tx = br.response().read()
 soup = BeautifulSoup(tx)
-try:
-  x = soup.findAll('p', class_='cat-lst')
-except:
-  x = ""
-for k in range(len(x)):
-  y = x[k].findAll('a')
-  for l in range(len(y)):
-    # print (y[j].decode_contents(formatter="html").split("</span>")[1],y[j]['href'])
-    br.open(y[l]['href'])
-    tx = br.response().read()
-    soup = BeautifulSoup(tx)
-    category = y[l].decode_contents(formatter="html").split("</span>")[1]
-    suppliers =  soup.find('p', class_='flt_wd').decode_contents(formatter="html").split(" ")[0]
-    print('Industrial Plant & Machine'+','+'Milling & Grinding Tools'+','+category+','+suppliers)
+a = soup.findAll('li', class_='q_cb')
+for j in range(2,20):
+  subcatergory = a[j].find('a').string;
+  br.open(a[j].find('a')['href'])
+  tx = br.response().read()
+  soup = BeautifulSoup(tx)
+  try:
+    x = soup.findAll('p', class_='cat-lst')
+  except:
+    x = ""
+  for k in range(len(x)):
+    y = x[k].findAll('a')
+    for l in range(len(y)):
+      # print (y[j].decode_contents(formatter="html").split("</span>")[1],y[j]['href'])
+      br.open(y[l]['href'])
+      tx = br.response().read()
+      soup = BeautifulSoup(tx)
+      category = y[l].decode_contents(formatter="html").split("</span>")[1]
+      suppliers =  soup.find('p', class_='flt_wd').decode_contents(formatter="html").split(" ")[0]
+      print('Industrial Plant & Machine'+','+subcatergory+','+category+','+suppliers)
