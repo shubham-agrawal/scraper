9146a9940d93f2319b86cd5aa6f08b95085936ad
shubham-agrawal <mail2shubham001@gmail.com>
2015-07-13 18:04:39 +0530

    manufacturing & assembling services

--- indiamarttest.py
+++ indiamarttest.py
@@ -24,36 +24,20 @@ br.set_handle_refresh(mechanize._http.HTTPRefreshProcessor(), max_time=1)
 
 # User-Agent (this is cheating, ok?)
 br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-br.open('http://dir.indiamart.com')
+br.open('http://www.sourbook.com/zipavuj.php?G7eeqpILGqJt8oq2cI0jxA=Q6tQjN5FbYJdlaouGd0SITZ4OBFGxjYJkz3oFMd1WM0wtJ%2BjekGIy2WqT%2FOqsafWLj5AajYdQKDkcUgQ9vD%2F0aIYiHlrvcc5BS%2FswHoIe28%3D')
 tx = br.response().read()
 soup = BeautifulSoup(tx)
-categories = soup.findAll('div', class_='catHd')
-data = []
-for i in range(len(categories)):
-  # print(categories[i].find('a').string ,categories[i].find('a')['href'] )
-  br.open(categories[i].find('a')['href'])
-  tx = br.response().read()
-  soup = BeautifulSoup(tx)
-  a = soup.findAll('li', class_='q_cb')
-  for j in range(len(a)):
-    subcatergory = a[j].find('a').string;
-    br.open(a[j].find('a')['href'])
+try:
+  x = soup.findAll('p', class_='cat-lst')
+except:
+  x = ""
+for k in range(len(x)):
+  y = x[k].findAll('a')
+  for l in range(len(y)):
+    # print (y[j].decode_contents(formatter="html").split("</span>")[1],y[j]['href'])
+    br.open(y[l]['href'])
     tx = br.response().read()
     soup = BeautifulSoup(tx)
-    try:
-      x = soup.findAll('p', class_='cat-lst')
-    except:
-      x = ""
-    for k in range(len(x)):
-      y = x[k].findAll('a')
-      for l in range(len(y)):
-        # print (y[j].decode_contents(formatter="html").split("</span>")[1],y[j]['href'])
-        br.open(y[l]['href'])
-        tx = br.response().read()
-        soup = BeautifulSoup(tx)
-        category = y[l].decode_contents(formatter="html").split("</span>")[1]
-        suppliers =  soup.find('p', class_='flt_wd').decode_contents(formatter="html").split(" ")[0]
-        print(categories[i].find('a').string+','+subcatergory+','+category+','+suppliers+'\n')
-        data.append(categories[i].find('a').string+','+subcatergory+','+category+','+suppliers+'\n')
-with open("output.csv", "wb") as f:
-  f.writelines(data)
+    category = y[l].decode_contents(formatter="html").split("</span>")[1]
+    suppliers =  soup.find('p', class_='flt_wd').decode_contents(formatter="html").split(" ")[0]
+    print('Industrial Plant & Machine'+','+'Manufacturing & Assembling Services'+','+category+','+suppliers)
